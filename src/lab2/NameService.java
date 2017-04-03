0c534da3ec929a5e530478c56adec464c8b6a497
kevinjerke <kevinjerke@10.0.1.30>
2017-04-03 10:37:57 -0500

    Completed all labs

--- src/lab2/NameService.java
+++ src/lab2/NameService.java
@@ -8,7 +8,11 @@ package lab2;
  * @version 1.00
  */
 public class NameService {
-    
+     private static final int FIRST_NAME_IDX = 0;
+     private static final int LAST_NAME_IDX = 1;
+     
+     
+     
     /**
      * Finds and returns the last name from within a full name. Caution: 
      * No validation is performed.
@@ -19,16 +23,21 @@ public class NameService {
      * fewer than two parts
      */
     public String extractLastName(String fullName) {
-        
+        if (fullName == null || fullName.isEmpty()) {
+            throw new IllegalArgumentException("Must enter a name");
+        } else {
         String[] nameParts = fullName.split(" ");
         if (nameParts.length > 2) {
             throw new IllegalArgumentException("Please enter in your first + last name");
+        } else {
+             return nameParts [nameParts.length - 1];
+          
         }
+       }
         // write your code here to extract the lastName and store in the
         // above local variable
-        
-        return nameParts [nameParts.length - 1];
     }
+     String lastName = null;
     
 
     
